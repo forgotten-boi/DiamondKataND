6a06bbde4dee11ff902f8630ece6a5184899e18d
Pitambar Jha <nishjha4u@gmail.com>
2024-10-06 18:23:40 +0100

    remove \n from the ending of expected result as per requirement

--- DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
+++ DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
@@ -8,20 +8,21 @@ public class DiamondPrinterTests
     public void TestDiamondsWithSingleLetter()
     {
         string result = DiamondPrinter.Create('A');
-        Assert.Equal($"A\n", result);
+        Assert.Equal($"A", result);
     }
 
     [Fact]
     public void TestDiamonsWithLetterB()
     {
         var result = DiamondPrinter.Create('B');
-        Assert.Equal(" A \nB B\n A \n", result);
+        Assert.Equal(" A \nB B\n A ", result);
     }
     [Fact]
     public void TestDiamondsWithLetterC()
     {
         var result = DiamondPrinter.Create('C');
-        Assert.Equal("  A\n B B \nC   C\n B B \n  A  \n", result);
+        Assert.Equal("  A  \n B B \nC   C\n B B \n  A  ", result);
+       
     }
 
 }
