a6f0df0d68e13be6b7c67e93abbc2c4cae82950f
Pitambar Jha <nishjha4u@gmail.com>
2024-10-06 17:17:03 +0100

    refactor test case based on requirement

--- DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
+++ DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
@@ -4,16 +4,10 @@ using DiamondPrinter.Core;
 
 public class DiamondPrinterTests
 {
-    [Theory]
-    [InlineData('A')]
-    [InlineData('B')]
-    [InlineData('C')]
-    [InlineData('D')]
-    [InlineData('E')]
-    [InlineData('F')]
-    public void TestDiamondsWithSingleLetter(char diamondLetter)
+    [Fact]
+    public void TestDiamondsWithSingleLetter()
     {
-        string result = DiamondPrinter.Create(diamondLetter);
-        Assert.Equal($"{diamondLetter}\n", result);
+        string result = DiamondPrinter.Create('A');
+        Assert.Equal($"A\n", result);
     }
 }
