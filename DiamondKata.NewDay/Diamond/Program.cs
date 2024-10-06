c379f078e4762f5771f1ae8dea464ecca955f05a
Pitambar Jha <nishjha4u@gmail.com>
2024-10-06 18:50:00 +0100

    console application to test output

---
+++ DiamondKata.NewDay/Diamond/Program.cs
@@ -0,0 +1,27 @@
+﻿// Test project to run from Command
+
+using System.Xml.Linq;
+
+var errorMsg = "The letter should be between A and Z";
+try
+{
+    string input = Environment.GetCommandLineArgs()[1];
+    if (string.IsNullOrEmpty(input) || input.Length != 1)
+    {
+        Console.WriteLine(errorMsg);
+        return;
+    }
+    var diamondStructure = DiamondPrinter.Core.DiamondPrinter.Create(input[0]);
+
+    Console.WriteLine(diamondStructure);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(errorMsg);
+}
+catch (Exception ex)
+{
+
+	throw;
+}
+
