cc479d1e89b1fff53f1bb81e0f24c63cf43d2b0c
Pitambar Jha <nishjha4u@gmail.com>
2024-10-06 16:24:00 +0100

    first test test diamond with single letter

---
+++ DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
@@ -0,0 +1,17 @@
+namespace DiamondPrinter.Tests;
+
+public class DiamondPrinterTests
+{
+    [Theory]
+    [InlineData("A")]
+    [InlineData("B")]
+    [InlineData("C")]
+    [InlineData("D")]
+    [InlineData("E")]
+    [InlineData("F")]
+    public void TestDiamondsWithSingleLetter(string diamondLetter)
+    {
+        string result = DiamondPrinter.Create(diamondLetter);
+        Assert.Equal($"{diamondLetter}\n", result);
+    }
+}
