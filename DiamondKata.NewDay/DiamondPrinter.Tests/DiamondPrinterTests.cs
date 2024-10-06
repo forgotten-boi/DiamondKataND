d62507edd5e7b071b9255d0087ae5351b354479c
Pitambar Jha <nishjha4u@gmail.com>
2024-10-06 17:28:36 +0100

    add test for diamond structure for letter c

--- DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
+++ DiamondKata.NewDay/DiamondPrinter.Tests/DiamondPrinterTests.cs
@@ -10,4 +10,18 @@ public class DiamondPrinterTests
         string result = DiamondPrinter.Create('A');
         Assert.Equal($"A\n", result);
     }
+
+    [Fact]
+    public void TestDiamonsWithLetterB()
+    {
+        var result = DiamondPrinter.Create('B');
+        Assert.Equal(" A \nB B\n A \n", result);
+    }
+    [Fact]
+    public void TestDiamondsWithLetterC()
+    {
+        var result = DiamondPrinter.Create('C');
+        Assert.Equal("  A\n B B \nC   C\n B B \n  A  \n", result);
+    }
+
 }
